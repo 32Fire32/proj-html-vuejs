942c8222553c36df1b26f777cac1dad2d221768a
32Fire32 <nicola.gianfelici@hotmail.it>
2022-12-06 12:44:37 +0100

    effetti hover sezione actions e projects

--- src/components/sections/SectionCards/AppCards3.vue
+++ src/components/sections/SectionCards/AppCards3.vue
@@ -11,9 +11,18 @@ export default {
 <template>
   <div class="card">
     <img :src="`src/assets/project-${img}.jpg`" :alt="title" />
-    <p>
-      {{ title }}
+    <a href="" class="hide"><i class="fa-solid fa-arrow-right"></i></a>
+    <p class="show">
+      <strong>{{ title }}</strong>
     </p>
+    <div class="hide">
+      <p>
+        <strong>{{ title }}</strong>
+      </p>
+      <p class="light-text">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      </p>
+    </div>
   </div>
 </template>
 
@@ -22,6 +31,13 @@ export default {
   width: calc(var(--container) / 3 - 40px);
   margin: 20px;
   position: relative;
+  a {
+    text-decoration: none;
+    color: #fff;
+    position: absolute;
+    top: 20px;
+    right: 20px;
+  }
   img {
     width: 100%;
     border-radius: 10px;
@@ -35,5 +51,30 @@ p {
   left: 20%;
   transform: translate(-10%, -50%);
   color: #fff;
+  font-size: 1.2rem;
+}
+.hide {
+  display: none;
+  p:first-child {
+    top: 65%;
+  }
+  p:last-child {
+    font-size: 0.8rem;
+  }
+}
+
+.card:hover {
+  cursor: pointer;
+}
+
+.card:hover img {
+  filter: blur(2px) brightness(40%);
+}
+.card:hover .hide {
+  display: block;
+}
+
+.card:hover .show {
+  display: none;
 }
 </style>
